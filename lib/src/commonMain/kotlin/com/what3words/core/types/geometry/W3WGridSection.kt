31f4bd9b11a3d675ceec67a4eeecadd3029e5a1a
Prosper <prosper.ekwerike@what3words.com>
2024-02-18 17:20:29 +0000

    Rename module from shared to lib

--- lib/src/commonMain/kotlin/com/what3words/core/types/geometry/W3WGridSection.kt
+++ lib/src/commonMain/kotlin/com/what3words/core/types/geometry/W3WGridSection.kt
@@ -2,9 +2,6 @@ package com.what3words.core.types.geometry
 
 import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
-import kotlinx.serialization.json.JsonArray
-import kotlinx.serialization.json.JsonBuilder
-import kotlinx.serialization.json.JsonObject
 import kotlinx.serialization.json.JsonPrimitive
 import kotlinx.serialization.json.buildJsonArray
 import kotlinx.serialization.json.buildJsonObject
