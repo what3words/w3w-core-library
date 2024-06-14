bf5a22fc279205e2eb1728e1d85a7d38c83e50f7
Emanuel Amiguinho <67689862+mani-w3w@users.noreply.github.com>
2024-06-14 12:14:19 +0100

    Revert "Epic/mt 729 core library"

--- lib/src/commonMain/kotlin/com/what3words/core/datasource/voice/audiostream/W3WMicrophone.kt
+++
@@ -1,7 +0,0 @@
-package com.what3words.core.datasource.voice.audiostream
-
-/**
- * Platform-specific implementation of [W3WAudioStream] interface, facilitating connection with the device
- * audio framework to capture and process audio signals from the device's microphone.
- */
-expect class W3WMicrophone : W3WAudioStream
