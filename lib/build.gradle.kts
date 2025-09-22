25aa8a7569da924d24a7870a61031c1f680f821d
Duy Phạm <duypham@MacBook-Pro-8.local>
2025-09-22 12:01:11 +0700

    Refactor publishing and remove CircleCI

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -1,6 +1,5 @@
+import org.jetbrains.kotlin.gradle.ExperimentalKotlinGradlePluginApi
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
-import org.jetbrains.kotlin.gradle.plugin.mpp.apple.XCFramework
-import java.net.URI
 
 plugins {
     id("maven-publish")
@@ -11,49 +10,21 @@ plugins {
     alias(libs.plugins.kotlinSerialization)
     alias(libs.plugins.jetbrainsCompose)
     alias(libs.plugins.compose.compiler)
+    alias(libs.plugins.vanniktech.mavenPublish)
 }
 
-group = "com.what3words"
-
-/**
- * IS_SNAPSHOT_RELEASE property will be automatically added to the root gradle.properties file by the CI pipeline, depending on the GitHub branch.
- * A snapshot release is generated for every pull request merged or commit made into an epic branch.
- */
-val isSnapshotRelease = findProperty("IS_SNAPSHOT_RELEASE") == "true"
-
-version =
-    if (isSnapshotRelease) "${findProperty("VERSION_NAME")}-SNAPSHOT" else "${findProperty("VERSION_NAME")}"
-
-@OptIn(org.jetbrains.kotlin.gradle.ExperimentalKotlinGradlePluginApi::class)
 kotlin {
-    applyDefaultHierarchyTemplate()
-    compilerOptions {
-        androidTarget {
-            compilerOptions.jvmTarget.set(JvmTarget.JVM_1_8)
-        }
-    }
-    jvm {
-        mavenPublication {
-            artifactId = "w3w-core-jvm"
-        }
-    }
+    jvm()
     androidTarget {
-        mavenPublication {
-            artifactId = "w3w-core-android"
-        }
-        publishLibraryVariants("release", "debug")
-    }
-    val xcFramework = XCFramework("W3WCore")
-    listOf(
-        iosX64(),
-        iosArm64(),
-        iosSimulatorArm64()
-    ).forEach {
-        it.binaries.framework {
-            baseName = "W3WCore"
-            xcFramework.add(this)
+        publishLibraryVariants("release")
+        @OptIn(ExperimentalKotlinGradlePluginApi::class)
+        compilerOptions {
+            jvmTarget.set(JvmTarget.JVM_11)
         }
     }
+    iosX64()
+    iosArm64()
+    iosSimulatorArm64()
 
     sourceSets {
         val commonMain by getting {
@@ -79,84 +50,42 @@ android {
     defaultConfig {
         minSdk = libs.versions.minSdk.get().toInt()
     }
-}
-
-val ossrhUsername = findProperty("OSSRH_USERNAME") as String?
-val ossrhPassword = findProperty("OSSRH_PASSWORD") as String?
-val signingKey = findProperty("SIGNING_KEY") as String?
-val signingKeyPwd = findProperty("SIGNING_KEY_PWD") as String?
-dependencies {
-    implementation(libs.ui.graphics.android)
-}
-
-
-afterEvaluate {
-    publishing {
-        repositories {
-            publications {
-                withType(MavenPublication::class.java) {
-                    if (name.contains("kotlinMultiplatform", ignoreCase = true)) {
-                        artifactId = "w3w-core-multiplatform"
-                    }
-                    val publicationName = name
-                    val dokkaJar =
-                        project.tasks.register("${publicationName}DokkaJar", Jar::class) {
-                            group = JavaBasePlugin.DOCUMENTATION_GROUP
-                            description = "Assembles Kotlin docs with Dokka into a Javadoc jar"
-                            archiveClassifier.set("javadoc")
-                            from(tasks.named("dokkaHtml"))
-
-                            // Each archive name should be distinct, to avoid implicit dependency issues.
-                            // We use the same format as the sources Jar tasks.
-                            // https://youtrack.jetbrains.com/issue/KT-46466
-                            archiveBaseName.set("${archiveBaseName.get()}-$publicationName")
-                        }
-                    artifact(dokkaJar)
-                    pom {
-                        name.set("w3w-core-library")
-                        description.set("Multiplatform library for what3words domain classes and interfaces.")
-                        url.set("https://github.com/what3words/w3w-core-library")
-                        licenses {
-                            license {
-                                name.set("The MIT License (MIT)")
-                                url.set("https://github.com/what3words/w3w-core-library/blob/master/LICENSE")
-                            }
-                        }
-                        developers {
-                            developer {
-                                id.set("what3words")
-                                name.set("what3words")
-                                email.set("development@what3words.com")
-                            }
-                        }
-                        scm {
-                            connection.set("scm:git:git://github.com/what3words/w3w-core-library.git")
-                            developerConnection.set("scm:git:ssh://git@github.com:what3words/w3w-core-library.git")
-                            url.set("https://github.com/what3words/w3w-core-library/tree/master")
-                        }
-                    }
-                }
-            }
-            maven {
-                name = "sonatype"
-                val releasesRepoUrl =
-                    "https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/"
-                val snapshotsRepoUrl =
-                    "https://s01.oss.sonatype.org/content/repositories/snapshots/"
-                url = if (version.toString()
-                        .endsWith("SNAPSHOT")
-                ) URI.create(snapshotsRepoUrl) else URI.create(releasesRepoUrl)
-
-                credentials {
-                    username = ossrhUsername
-                    password = ossrhPassword
-                }
-            }
-        }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
     }
 }
 
-signing {
-    useInMemoryPgpKeys(signingKey, signingKeyPwd)
-    sign(publishing.publications)
+mavenPublishing {
+    publishToMavenCentral()
+
+    signAllPublications()
+
+    coordinates(libs.versions.groupId.get(), libs.versions.artifactId.get())
+
+    pom {
+        name = "what3words core library"
+        description = "Multiplatform library for what3words domain classes and interfaces."
+        inceptionYear = "2024"
+        url = "https://github.com/what3words/w3w-core-library"
+        licenses {
+            license {
+                name = "MIT License"
+                url = "https://github.com/what3words/w3w-core-library/blob/master/LICENSE"
+                distribution = "https://www.opensource.org/licenses/mit-license.php"
+            }
+        }
+        developers {
+            developer {
+                id = "what3words"
+                name = "what3words"
+                url = "development@what3words.com"
+            }
+        }
+        scm {
+            url = "https://github.com/what3words/w3w-core-library/tree/master"
+            connection = "scm:git:git://github.com/what3words/w3w-core-library.git"
+            developerConnection = "scm:git:ssh://git@github.com:what3words/w3w-core-library.git"
+        }
+    }
 }
